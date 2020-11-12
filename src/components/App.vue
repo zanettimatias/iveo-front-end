058d7e11a575a64a2b589c411d9b39d99fce7510
mzanetti <zanetti.matias@gmail.com>
2020-11-11 23:33:29 -0300

    commit inicial

---
+++ src/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <Page>
+        <ActionBar title="Bienvenidos a  IVEO"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>s
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>

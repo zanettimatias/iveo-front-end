74b731ecbdfd0a6a0f4ebbdfa0ad52cf17f5d825
pejelagarto <zanetti.matias@gmail.com>
2021-09-29 22:38:44 -0300

    -Se tuvo que migrar todo el entorno para la nueva version de nativescript

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core'
+import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.application',
-  appPath: 'src',
+  id: 'ar.com.mzanetti.iveo',
+  appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none'
   }
-} as NativeScriptConfig
+} as NativeScriptConfig;
